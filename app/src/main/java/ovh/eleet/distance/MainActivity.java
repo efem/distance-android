7088ac54e58ea4ec7ec0413d803b94e54b85eb4f
efem <qlkid@o2.pl>
2017-05-19 23:35:01 +0200

    Fabs and background

--- app/src/main/java/ovh/eleet/distance/MainActivity.java
+++ app/src/main/java/ovh/eleet/distance/MainActivity.java
@@ -1,5 +1,6 @@
 package ovh.eleet.distance;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
@@ -8,13 +9,17 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -24,10 +29,15 @@ import android.widget.Toast;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import jp.wasabeef.blurry.Blurry;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 public class MainActivity extends AppCompatActivity {
 
 
     LinearLayout linLayMain;
+    static boolean fabClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +45,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final CoordinatorLayout colLay = (CoordinatorLayout) findViewById(R.id.coordLay);
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final FloatingActionButton fabSmall1 = (FloatingActionButton) findViewById(R.id.fabSmall1);
+        final FloatingActionButton fabSmall1 = (FloatingActionButton) findViewById(R.id.fabSmallRange);
         final CoordinatorLayout mainCoord = (CoordinatorLayout) findViewById(R.id.mainCoord);
-        ScrollView sv = (ScrollView) findViewById(R.id.sViewMain);
+        final ScrollView sv = (ScrollView) findViewById(R.id.sViewMain);
+        final RelativeLayout mainRelLay = (RelativeLayout) findViewById(R.id.include);
+
+
         sv.setOnScrollChangeListener(new View.OnScrollChangeListener() {
             @Override
             public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
                 if (scrollY > 0 ||scrollY <0 && fab.isShown()) {
                     fab.hide();
-                    colLay.setVisibility(View.GONE);
+                    colLay.setVisibility(View.INVISIBLE);
+
                 }
                 else
+
                     fab.show();
             }
 
@@ -54,22 +69,31 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),
-                        "Button is clicked", Toast.LENGTH_LONG).show();
-                /*if(fabSmall1.getVisibility()==View.INVISIBLE)
-                    fabSmall1.setVisibility(View.VISIBLE);
-                else if(fabSmall1.getVisibility()==View.VISIBLE)
-                    fabSmall1.setVisibility(View.INVISIBLE);*/
-                mainCoord.setBackgroundColor(Color.parseColor("#AAB6C7"));
-                if(colLay.getVisibility()==View.GONE)
+
+                if(colLay.getVisibility()==View.INVISIBLE) {
                     colLay.setVisibility(View.VISIBLE);
-                else if(colLay.getVisibility()==View.VISIBLE)
-                    colLay.setVisibility(View.GONE);
-                fab.setImageDrawable(getResources().getDrawable(R.drawable.ic_clear_white_24px));
-                mainCoord.getBackground().setAlpha(50);
+                    fabClicked = true;
+                }
+                else if(colLay.getVisibility()==View.VISIBLE) {
+                    colLay.setVisibility(View.INVISIBLE);
+                    fabClicked = false;
+                }
+                if (fabClicked) {
+                    fab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_clear_white_24px));
+                    mainCoord.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroundDarker));
+                    colLay.bringToFront();
+                }
+                else{
+                    fab.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_add_white_24px));
+                    mainCoord.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroundMain));
+                }
+                //mainCoord.getBackground().setAlpha(50);
+
             }
 
         });
+
+
     }
 
     @Override
@@ -126,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
             TableLayout tableLayout = new TableLayout(MainActivity.this);
             TableRow tableRow;
 
+            Resources resource = getResources();
 
             for (Record r : RECLIST) {
                 tableRow = new TableRow(MainActivity.this);
@@ -133,10 +158,14 @@ public class MainActivity extends AppCompatActivity {
                 TextView tvDate = new TextView(MainActivity.this);
                 TextView tvDistance = new TextView(MainActivity.this);
 
+                tvDistance.setTextColor(getColor(R.color.fontColorMain));
                 tvDistance.setText(String.valueOf(r.getDistance()) + " cm");
 
+                tvDate.setTextColor(getColor(R.color.fontColorMain));
                 tvDate.setText(df.format(r.getDate()));
                 tvDate.setPadding(20, 20, 20, 20);
+                tvDate.setWidth((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 180, resource.getDisplayMetrics()));
+
 
                 tableRow.addView(tvDate);
                 tableRow.addView(tvDistance);
@@ -146,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("r: " + r.toString());
             }
             linLayMain.addView(tableLayout);
+
 
         }
 
